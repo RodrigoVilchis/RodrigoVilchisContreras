b230ecd2993133854f92c20f1006df706708d635
RodrigoVilchis <103169401+RodrigoVilchis@users.noreply.github.com>
2022-05-03 18:00:58 -0500

    Update servExp.js

--- Entregable3/servExp.js
+++ Entregable3/servExp.js
@@ -1,24 +1,47 @@
 //npm init "en el terminal"
 //npm install express
-
 const express = require("express")
+const modulo = require("./ejercicioclase3")
 
-const { Contenedor } = require("../ejercicioclase3.js")
+let file = "./producto.txt";
+const producto = new modulo.Contenedor(file)
 
-const producto = new Contenedor("../producto.txt")
-
-const PORT = 8080
-
+const PORT = process.env.PORT || 8080
 const app = express()
+
 
 const server = app.listen(PORT, () => {
     console.log("Servidor HTTP escuchando en el puerto: " + PORT)
-    producto.save({title: "Ejemplo", price: 333, thumbnail: "https://libro-ejemplo/book"});
-    producto.getById(1)
+
 })
-app.get("/", (req, res) => {
-    res.send("../producto.txt")
+app.get('/', (req, res) => {
+    res.send('<h1 style="color:blue;">Bienvenidos al servidor express</h1>')
 })
+
+app.get("/productos", async(req, res) => {
+    const productos = await producto.getAll()
+    //console.log(productos)
+
+    res.status(200).send(productos)
+})
+
+app.get("/productoRandom", (req, res) =>{
+    producto.getAll().then(productos => {
+        const parseProducts = JSON.parse(productos)
+        const tamanio = parseProducts.length
+        const select = Math.ceil(Math.random() *tamanio-1)
+        //console.log(select)
+        //console.log(tamanio)
+        //console.log(parseProducts)
+        res.send(parseProducts[select])
+        
+    }) .catch(err => {
+        console.error(err)
+        res.status(500).send("Fallo del Servidor")
+    })
+    
+})
+
 
 // GET
 // POST
